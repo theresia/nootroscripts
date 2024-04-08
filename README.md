f3fe53f329be10978c3375437b768eefd6b6f6ab
Theresia Tanzil <tre.theresia@gmail.com>
2024-04-08 14:53:32 +0700

    no message

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# nootroscripts
+For context, see blogpost: http://proses.id/nootroscripts/
+
+For usage,
+
+Set up your virtual env (or not, if you like living dangerously).
+
+pip install -r requirements.txt
+
+put OPENAI_KEY in your env path
---
+++ test.sh
@@ -0,0 +1,14 @@
+./url2md.py --url https://investorplace.com/2024/04/3-stocks-to-ditch-following-the-april-bitcoin-halving/
+./url2md.py --url https://jalammar.github.io/illustrated-transformer/                                     
+./url2md.py --url https://thereader.mitpress.mit.edu/noam-chomsky-and-andrea-moro-on-the-limits-of-our-comprehension/ 
+
+./md2notes.py output/mds/thereader.mitpress.mit.edu/noam-chomsky-and-andrea-moro-on-the-limits-of-our-comprehension.md
+./md2notes.py output/mds/thereader.mitpress.mit.edu/noam-chomsky-and-andrea-moro-on-the-limits-of-our-comprehension.md --lmodel mistral
+
+./audio2llm.py --af sample/test_sound.m4a
+./audio2llm.py --tf output/test_sound.txt --lmodel mistral
+
+./youtube2llm.py summarise --vid=8Lzo3nhcTSk --nc
+./youtube2llm.py summarise --vid=8Lzo3nhcTSk --nc --lmodel mistral
+./youtube2llm.py embed --tf output/8Lzo3nhcTSk-transcript.txt 
+./youtube2llm.py ask --ef output/embeddings/8Lzo3nhcTSk-transcript-transcript_embedding.csv 
